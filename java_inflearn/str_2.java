402719930a8684002ce41926956f7023aecd5857
staylee <staylee0915@gmail.com>
2025-08-08 22:17:26 +0900

    commit

---
+++ java_inflearn/str_2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class str_2 {
+    public static void main(String[] args) {
+        //입력을 받고 대문자는 소문자로, 소문자는 대문자로 변환
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
